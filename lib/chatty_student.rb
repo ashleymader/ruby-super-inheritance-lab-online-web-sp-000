348b69691cbb34c0e03d650e7c79406555f5a300
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-18 15:44:48 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,6 +8,6 @@ class ChattyStudent < Student
   
   def raise_hand 
     super
-    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+    return "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
   end
 end
